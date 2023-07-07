e945de2430491d94c12b4ffccc929e04d3f4cae9
Michael Plate <mp@jamoin.de>
2023-07-07 21:13:31 +0200

    do stuff

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 
-import { CrtWrapperComponent } from './crt-wrapper/crt-wrapper.component';
 import { BoxComponent } from './box/box.component';
 
 @NgModule({
   imports: [
     BoxComponent,
-    CrtWrapperComponent,
   ],
   exports: [
     BoxComponent,
-    CrtWrapperComponent,
   ],
 })
 export class SharedModule { }
