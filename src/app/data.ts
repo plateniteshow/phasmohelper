c44718a1440f9258420b8ef49762be332ea95ca0
Michael Plate <mp@jamoin.de>
2023-07-11 16:23:33 +0200

    update ghost data

--- src/app/data.ts
+++ src/app/data.ts
@@ -7,10 +7,7 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.EMF5, Evidence.SPIRITBOX, Evidence.WRITING],
     huntSanity: 50,
     huntSpeed: [1.7],
-    tells: ['lorem ipsum'],
-    behaviors: ['lorem ipsum'],
-    huntSanityInfo: 'lorem ipsum',
-    huntSpeedInfo: 'lorem ipsum',
+    tells: ['Smudging the ghost will prevent a hunt for 180s instead of the normal 90s'],
   },
   // Wraith
   {
@@ -18,6 +15,8 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.EMF5, Evidence.SPIRITBOX],
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: ['Ghost will never step in salt'],
+    behaviors: ['Can teleport to random players, leaving EMF 2 or EMF 5'],
   },
   // Phantom
   {
@@ -25,6 +24,12 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.FINGERPRINTS, Evidence.SPIRITBOX],
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: [
+      'Does not appear in ghost photo',
+      'During an event, taking a photo will cause the ghost to disappear',
+      'Less visible during hunts',
+    ],
+    behaviors: ['Can roam to a random player, leaving EMF 2'],
   },
   // Poltergeist
   {
@@ -32,6 +37,14 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.SPIRITBOX, Evidence.WRITING],
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: [
+      'Can throw multiple objects at once, decreasing player sanity 2% per item thrown',
+      'Will throw an item every 0.5s during a hunt',
+    ],
+    behaviors: [
+      'Higher chance to throw & interact with objects',
+      'Can throw objects faster and further'
+    ],
   },
   // Banshee
   {
@@ -39,6 +52,13 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.FINGERPRINTS, Evidence.ORBS],
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: ['Banshee scream through paramic'],
+    behaviors: [
+      'Will only go after its target during a hunt (unless target is outside)',
+      'Will roam toward its target, without leaving EMF 2',
+      'Prefers singing ghost events',
+    ],
+    huntSanityInfo: 'Hunts when target\'s sanity is 50% or lower (even if target is outside)',
   },
   // Jinn
   {
@@ -46,6 +66,9 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.EMF5, Evidence.FINGERPRINTS, Evidence.FREEZING],
     huntSanity: 50,
     huntSpeed: [1.7, 2.5],
+    tells: ['Ability will drop nearby players\' sanity by 25% with EMF 2 at the breaker'],
+    behaviors: ['Can not turn off fuse box'],
+    huntSpeedInfo: '2.5m/s when fuse box is on and ghost is further than 3m from a seen player, 1.7m/s otherwise',
   },
   // Mare
   {
@@ -53,6 +76,12 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.ORBS, Evidence.SPIRITBOX, Evidence.WRITING],
     huntSanity: 60,
     huntSpeed: [1.7],
+    tells: [
+      'Ability to immediately turn off a light that a player has turned on',
+      'Can not turn on lights',
+    ],
+    behaviors: ['More likely to turn off and bust lights'],
+    huntSanityInfo: '60% when lights are off in the ghost\'s current room, 40% when on',
   },
   // Revenant
   {
@@ -60,7 +89,7 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.FREEZING, Evidence.ORBS, Evidence.WRITING],
     huntSanity: 50,
     huntSpeed: [1.0, 3.0],
-    // does not speed up
+    huntSpeedInfo: '3.0m/s immediately once the player has been sensed (equipment, voice, or line of sight) and until ghost has reached the players last known location where it will gradually slow down to 1m/s',
   },
   // Shade
   {
@@ -68,6 +97,15 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.EMF5, Evidence.FREEZING, Evidence.WRITING],
     huntSanity: 35,
     huntSpeed: [1.7],
+    tells: [
+      'Does not do interactions that result in EMF 2, 3, or 5 with someone in the room (except writing)',
+      'Will not hunt with someone in the room',
+    ],
+    behaviors: [
+      'More ghost events at lower sanity',
+      'Less likely to appear as full ghost model',
+      'Has a chance to appear as a shadow if summoned from music box or summoning circle',
+    ],
   },
   // Demon
   {
@@ -75,6 +113,12 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.FINGERPRINTS, Evidence.FREEZING, Evidence.WRITING],
     huntSanity: 70,
     huntSpeed: [1.7],
+    tells: [
+      'Smudging the ghost will prevent a hunt for 60s instead of the normal 90s',
+      '20s minimum hunt cool-down (normal is 25s)',
+    ],
+    behaviors: ['Crucifix range effect increased to 5m instead of 3m'],
+    huntSanityInfo: 'Ability to hunt at any sanity, hunts normally at 70%',
   },
   // Yurei
   {
@@ -82,6 +126,11 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.FREEZING, Evidence.ORBS],
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: [
+      'Can shut a door and drop sanity of nearby players by 15% if a door is in the room',
+      'Only ghost that can close an exit door outside of a hunt or ghost event',
+    ],
+    behaviors: ['Smudge stops wandering for 90 seconds'],
   },
   // Oni
   {
@@ -89,6 +138,12 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.EMF5, Evidence.FREEZING],
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: [
+      'Drains 20% on ghost events, instead of 10%',
+      'Will not do ghost mist event',
+      'More visible during hunts',
+    ],
+    behaviors: ['More active around multiple people'],
   },
   // Yokai
   {
@@ -96,6 +151,9 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.ORBS, Evidence.SPIRITBOX],
     huntSanity: 80,
     huntSpeed: [1.7],
+    tells: ['Hearing/electronic distance is less than 2.5m during hunts'],
+    behaviors: ['More active when talked to'],
+    huntSanityInfo: 'Can hunt up to 80% sanity when talking near it, hunts normally at 50%',
   },
   // Hantu
   {
@@ -104,7 +162,12 @@ export const GHOSTS: Ghost[] = [
     forcedEvidence: Evidence.FREEZING,
     huntSanity: 50,
     huntSpeed: [1.44, 1.7, 2.7],
-    // does not speed up
+    tells: [
+      'Ghost can have visible freezing breath in any room during hunts with the breaker off',
+      'Cannot turn breaker on',
+    ],
+    behaviors: ['High chance to turn off breaker'],
+    huntSpeedInfo: 'Faster in colder temperatures, does not speed up in line-of-sight',
   },
   // Goryo
   {
@@ -113,6 +176,11 @@ export const GHOSTS: Ghost[] = [
     forcedEvidence: Evidence.DOTS,
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: ['DOTs only appear on camera and will rarely show if player is in the same room'],
+    behaviors: [
+      'Can not change favorite rooms',
+      'Does not roam long distances',
+    ],
   },
   // Myling
   {
@@ -120,6 +188,8 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.EMF5, Evidence.FINGERPRINTS, Evidence.WRITING],
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: ['Footsteps and vocals cannot be heard more than 12m away during hunts (normal is 20m)'],
+    behaviors: ['Lots of paranormal sounds through paramic'],
   },
   // Onryo
   {
@@ -127,6 +197,12 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.FREEZING, Evidence.ORBS, Evidence.SPIRITBOX],
     huntSanity: 60,
     huntSpeed: [1.7],
+    tells: ['Will hunt at any sanity after extinguishing every 3rd flame if no other flames or crucifix will prevent it'],
+    behaviors: [
+      'Flames act like crucifixes, will blow out a flame if it tries to hunt (within 4m)',
+      'More likely to extinguish a flame, the more players that are dead',
+    ],
+    huntSanityInfo: 'Can hunt at any sanity using its ability, hunts normally at 60%',
   },
   // The Twins
   {
@@ -134,6 +210,9 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.EMF5, Evidence.FREEZING, Evidence.SPIRITBOX],
     huntSanity: 50,
     huntSpeed: [1.53, 1.87],
+    tells: ['Often do interactions at the same time'],
+    behaviors: ['Decoy only does interactions/EMF, no evidence or sensors'],
+    huntSpeedInfo: 'Either 1.53m/s or 1.87m/s depending on which twin hunted',
   },
   // Raiju
   {
@@ -141,6 +220,9 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.EMF5, Evidence.ORBS],
     huntSanity: 65,
     huntSpeed: [1.7, 2.5],
+    tells: ['During a hunt, flickers electronics at 15m range instead of 10m'],
+    huntSanityInfo: 'Can hunt at 65% sanity near active player equipment',
+    huntSpeedInfo: '2.5m/s near active player equipment, 1.7m/s otherwise',
   },
   // Obake
   {
@@ -149,6 +231,12 @@ export const GHOSTS: Ghost[] = [
     forcedEvidence: Evidence.FINGERPRINTS,
     huntSanity: 50,
     huntSpeed: [1.7],
+    tells: [
+      'Special 6 fingered fingerprints',
+      'Has a 6.66% chance to change ghost models when flickering during hunts (guaranteed once per hunt, returns to original model)',
+      'Does not always leave fingerprints',
+      'Can make fingerprints disappear earlier than 2 minutes',
+    ],
   },
   // The Mimic
   {
@@ -156,6 +244,13 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.FINGERPRINTS, Evidence.FREEZING, Evidence.SPIRITBOX, Evidence.ORBS],
     huntSanity: 50,
     huntSpeed: [1.0, 1.7, 2.75],
+    tells: [
+      'Always shows orbs as additional fake evidence (4th evidence) even in 0 evidence',
+      'Copies the behavior and traits of other ghosts',
+      'Changes ghost behavior every 30 seconds to 2 minutes',
+    ],
+    huntSanityInfo: 'Copies behavior of currently mimicked ghost',
+    huntSpeedInfo: 'Copies behavior of currently mimicked ghost',
   },
   // Moroi
   {
@@ -164,6 +259,12 @@ export const GHOSTS: Ghost[] = [
     forcedEvidence: Evidence.SPIRITBOX,
     huntSanity: 50,
     huntSpeed: [1.5, 1.7, 2.25],
+    tells: [
+      'Places curse on player when heard through paramic or spirit box, curse drops sanity 2x as fast',
+      'Holding a lit candle will not prevent sanity drain if cursed',
+      'During a hunt, smudging the ghost will blind it for 12s (normal is 6s)',
+    ],
+    huntSpeedInfo: 'Faster when average sanity is lower, can reach a speed of 3.71 m/s when in line of sight',
   },
   // Deogen
   {
@@ -172,7 +273,12 @@ export const GHOSTS: Ghost[] = [
     forcedEvidence: Evidence.SPIRITBOX,
     huntSanity: 40,
     huntSpeed: [0.4, 3.0],
-    // does not speed up
+    tells: [
+      'Heavy breathing through spirit box when within 1m of the ghost',
+      'Cannot hide from a Deogen',
+    ],
+    behaviors: ['Increased chance for DOTs / Writing'],
+    huntSpeedInfo: '3.0m/s when far away, drops to 0.4m/s when close to the player',
   },
   // Thaye
   {
@@ -180,6 +286,12 @@ export const GHOSTS: Ghost[] = [
     evidences: [Evidence.DOTS, Evidence.ORBS, Evidence.WRITING],
     huntSanity: 75,
     huntSpeed: [1.0, 1.7, 2.75],
-    // does not speed up
+    behaviors: [
+      'Ghost ages over time (every 1-2 minutes) while player is nearby',
+      'More active when younger',
+      'Increased chance for DOTs / Writing',
+    ],
+    huntSanityInfo: 'Hunts at 75% at its youngest, 15% at its oldest',
+    huntSpeedInfo: '2.75m/s at its youngest, 1.0m/s at its oldest. Does not speed up in line-of-sight'
   },
 ];
