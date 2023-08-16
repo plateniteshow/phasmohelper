b18e548176b4df113f890b56259b69c4c17931ec
Michael Plate <mp@jamoin.de>
2023-08-16 10:53:21 +0200

    add voice to smudge timer

--- src/app/features/smudge-timer/smudge-timer.component.ts
+++ src/app/features/smudge-timer/smudge-timer.component.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import Speech from "speak-tts";
+
 import { Component, HostBinding } from '@angular/core';
 
 @Component({
@@ -12,6 +15,18 @@ export class SmudgeTimerComponent {
   public timer: number = this.timerMax;
 
   private smudgeTimer!: NodeJS.Timer;
+  private readonly speech: any;
+
+  constructor() {
+    const speech = new Speech();
+    speech.init({
+      volume: 0.25,
+      lang: "en-GB",
+      rate: 1,
+      pitch: 1,
+    });
+    this.speech = speech;
+  }
 
   @HostBinding("style.--progress")
   public get progress(): string {
@@ -20,12 +35,50 @@ export class SmudgeTimerComponent {
 
   public startTimer = () => {
     this.running = true;
+
     this.smudgeTimer = setInterval(() => {
       this.timer -= 1000;
+
+      // Demon Smudge
+      this.setCountDown('Demon Smudge', this.timer, 170, 5);
+
+      // Default Smudge
+      this.setCountDown('Default Smudge', this.timer, 90, 5);
+
+      // Spirit Smudge
+      this.setCountDown('Spirit Smudge', this.timer, 0, 5);
+
       if (this.timer === 0) {
-        clearInterval(this.smudgeTimer);
+        this.stopTimer();
       }
     }, 1000);
+  }
+
+  public setCountDown = (text: string, currentTime: number, countDownEnd: number, countdownLength: number) => {
+    if (countdownLength === 0) {
+      return;
+    }
+
+    // Start speech
+    if (currentTime / 1000 === countDownEnd + 7) {
+      this.speech
+        .speak({
+          text: text + ' ending in:',
+        });
+    }
+
+    // Start countdown speech
+    if (currentTime / 1000 === countDownEnd + countdownLength + 1) {
+      let countdown = countdownLength;
+      const interval = setInterval(() => {
+        this.speech.speak({ text: `${countdown}` });
+        countdown--;
+        if (countdown === 0) {
+          clearInterval(interval);
+        }
+      }, 1000);
+    }
+
   }
 
   public stopTimer = () => {
