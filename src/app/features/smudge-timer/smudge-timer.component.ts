cdc9da818bcf55bbdc9dc17272924af89ff70f63
Michael Plate <mp@jamoin.de>
2023-08-16 11:16:09 +0200

    adjust css marker positions to js timestamps

--- src/app/features/smudge-timer/smudge-timer.component.ts
+++ src/app/features/smudge-timer/smudge-timer.component.ts
@@ -9,17 +9,21 @@ import { Component, HostBinding, Input } from '@angular/core';
   styleUrls: ['./smudge-timer.component.scss']
 })
 export class SmudgeTimerComponent {
+  public readonly defaultTimestamp = 90 * 1000;
+  public readonly demonTimestamp = 120 * 1000;
+  public readonly spiritTimestamp = 0;
+  public readonly timerMax = 180 * 1000;
+
   /** Whether the smudge timer is allowed to play sounds or not */
   @Input()
   public mute = false;
 
-  public readonly timerMax = 180 * 1000;
-
   public running = false;
   public timer: number = this.timerMax;
 
-  private smudgeTimer!: NodeJS.Timer;
   private readonly speech: any;
+
+  private smudgeTimer!: NodeJS.Timer;
 
   constructor() {
     const speech = new Speech();
@@ -32,36 +36,33 @@ export class SmudgeTimerComponent {
     this.speech = speech;
   }
 
+  @HostBinding("style.--defaultMarkerPosition")
+  public get defaultMarkerPosition(): string {
+    return (this.defaultTimestamp / this.timerMax * 100) + "%";
+  }
+
+  @HostBinding("style.--demonMarkerPosition")
+  public get demonMarkerPosition(): string {
+    return (this.demonTimestamp / this.timerMax * 100) + "%";
+  }
+
   @HostBinding("style.--progress")
   public get progress(): string {
     return (this.timer / this.timerMax * 100) + "%";
   }
 
-  public startTimer = () => {
-    this.running = true;
-
-    this.smudgeTimer = setInterval(() => {
-      this.timer -= 1000;
-
-      if (!this.mute) {
-        this.setCountDown('Demon Smudge', this.timer, 120, 5);
-        this.setCountDown('Default Smudge', this.timer, 90, 5);
-        this.setCountDown('Spirit Smudge', this.timer, 0, 5);
-      }
-
-      if (this.timer === 0) {
-        this.stopTimer();
-      }
-    }, 1000);
+  @HostBinding("style.--spiritMarkerPosition")
+  public get spiritMarkerPosition(): string {
+    return (this.spiritTimestamp / this.timerMax * 100) + "%";
   }
 
   /**
    * Starts a spoken countdown.
    *
    * @param text What to say when the countdown starts
-   * @param currentTime
-   * @param countDownEnd
-   * @param countdownLength
+   * @param currentTime in ms
+   * @param countDownEnd in ms
+   * @param countdownLength in ms
    */
   public setCountDown = (text: string, currentTime: number, countDownEnd: number, countdownLength: number): void => {
     if (countdownLength === 0) {
@@ -69,7 +70,7 @@ export class SmudgeTimerComponent {
     }
 
     // Start speech
-    if (currentTime / 1000 === countDownEnd + 7) {
+    if (currentTime === countDownEnd + 7000) {
       this.speech
         .speak({
           text: text + ' ending in:',
@@ -77,8 +78,8 @@ export class SmudgeTimerComponent {
     }
 
     // Start countdown speech
-    if (currentTime / 1000 === countDownEnd + countdownLength + 1) {
-      let countdown = countdownLength;
+    if (currentTime === countDownEnd + countdownLength + 1000) {
+      let countdown = countdownLength / 1000;
       const interval = setInterval(() => {
         // If the countdown has run out OR the timer has been stopped, clear this interval.
         if (countdown === 0 || !this.running) {
@@ -89,7 +90,24 @@ export class SmudgeTimerComponent {
         countdown--;
       }, 1000);
     }
+  }
 
+  public startTimer = () => {
+    this.running = true;
+
+    this.smudgeTimer = setInterval(() => {
+      this.timer -= 1000;
+
+      if (!this.mute) {
+        this.setCountDown('Demon Smudge', this.timer, this.demonTimestamp, 5000);
+        this.setCountDown('Default Smudge', this.timer, this.defaultTimestamp, 5000);
+        this.setCountDown('Spirit Smudge', this.timer, this.spiritTimestamp, 5000);
+      }
+
+      if (this.timer === 0) {
+        this.stopTimer();
+      }
+    }, 1000);
   }
 
   public stopTimer = () => {
