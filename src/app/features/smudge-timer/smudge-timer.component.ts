b487f15bddb51f01ef4529485bc2452919c9d8b2
Michael Plate <mp@jamoin.de>
2023-08-16 09:31:32 +0200

    add smudge timer

--- src/app/features/smudge-timer/smudge-timer.component.ts
+++ src/app/features/smudge-timer/smudge-timer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'smudge-timer',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./smudge-timer.component.scss']
 })
 export class SmudgeTimerComponent {
+  public readonly timerMax = 180 * 1000;
 
+  public running = false;
+  public timer: number = this.timerMax;
+
+  private smudgeTimer!: NodeJS.Timer;
+
+  @HostBinding("style.--progress")
+  public get progress(): string {
+    return (this.timer / this.timerMax * 100) + "%";
+  }
+
+  public startTimer = () => {
+    this.running = true;
+    this.smudgeTimer = setInterval(() => {
+      this.timer -= 1000;
+      if (this.timer === 0) {
+        clearInterval(this.smudgeTimer);
+      }
+    }, 1000);
+  }
+
+  public stopTimer = () => {
+    this.running = false;
+    this.timer = this.timerMax;
+    clearInterval(this.smudgeTimer);
+  }
 }
